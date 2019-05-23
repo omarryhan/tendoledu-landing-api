1da112e0452d904134b22d326945fbadb921ddc9
Omar Ryhan <omarryhan@gmail.com>
2019-05-23 10:06:43 +0200

    Finally got Aiogoogle to work with Firestore

--- app.py
+++ app.py
@@ -1,2 +1,92 @@
-from sanic import Sanic
+import datetime
 
+from sanic import Sanic, response
+from secure import SecureHeaders, SecurePolicies
+from aiogoogle import Aiogoogle as Aiogoogle_
+import yaml
+
+
+app = Sanic()
+
+with open("keys.yaml", "r") as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
+
+user_creds = {
+    'access_token': config["user_creds"]["access_token"],
+    'refresh_token': config["user_creds"]["refresh_token"],
+    'expires_at': config["user_creds"]["expires_at"],
+}
+
+client_creds = {
+    'client_id': config["client_creds"]['client_id'],
+    'client_secret': config["client_creds"]['client_secret'],
+}
+
+Aiogoogle = lambda: Aiogoogle_(user_creds=user_creds, client_creds=client_creds)
+
+recaptcha_private_key = config['recaptcha_private_key']
+
+firestore = None
+
+def secure_app():
+    secure_headers = SecureHeaders()
+    async def secure(request, response):
+        secure_headers.sanic(response)
+    app.register_middleware(secure, 'response')
+
+secure_app()
+
+@app.listener('before_server_start')
+async def discover_firestore(app, loop):
+    global firestore
+    async with Aiogoogle() as google:
+        firestore = await google.discover('firestore', 'v1')
+
+@app.route('/signup/schools', methods=['POST'])
+async def schools_signup_handler(request):
+    pass
+
+@app.route('/signup/students', methods=['POST'])
+async def students_signup_handler(request):
+    pass
+
+@app.route('/signup/startups', methods=['POST'])
+async def startups_signup_handler(request):
+    pass
+
+@app.route('/test')
+async def test(request):
+    email = 'test@example.com'
+    expertise = 'being a bot'
+    page_name = 'test'
+    async with Aiogoogle() as google:
+        resp = await google.as_user(
+            firestore.projects.databases.documents.createDocument(
+                parent='projects/tendoledu/databases/(default)/documents',
+                collectionId='signups',
+                json=dict(fields={
+                    'email': {
+                        'stringValue': email,
+                    },
+                    'addedAt': {
+                        'timestampValue': datetime.datetime.utcnow().isoformat() + 'Z',
+                    },
+                    'pageName': {
+                        'stringValue': page_name,
+                    },
+                    'expertise': {
+                        'stringValue': expertise,
+                    },
+                }),
+                validate=False,  # "parent" validation has a wrong pattern. Our pattern works fine.
+            ),
+            user_creds=user_creds,
+        )
+
+    return response.json(resp)
+
+@app.route('/')
+async def home(request):
+    return response.json(firestore.discovery_document)
+
+app.go_fast(port=7000)
---
+++ oauth2.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3.7
+
+import sys, os, webbrowser, yaml, asyncio, pprint
+
+from aiogoogle import Aiogoogle
+from aiogoogle.excs import HTTPError
+from aiogoogle.auth.utils import create_secret
+from aiogoogle.auth.managers import OOB_REDIRECT_URI
+
+try:
+    with open("keys.yaml", "r") as stream:
+        config = yaml.load(stream)
+except Exception as e:
+    print("Rename _keys.yaml to keys.yaml")
+    raise e
+
+EMAIL = config["user_creds"]["email"]
+CLIENT_CREDS = {
+    "client_id": config["client_creds"]["client_id"],
+    "client_secret": config["client_creds"]["client_secret"],
+    "scopes": config["client_creds"]["scopes"],
+    "redirect_uri": OOB_REDIRECT_URI,
+}
+
+
+async def main():
+    aiogoogle = Aiogoogle(client_creds=CLIENT_CREDS)
+    uri = aiogoogle.oauth2.authorization_url(
+        client_creds=CLIENT_CREDS,
+        access_type="offline",
+        include_granted_scopes=True,
+        prompt="select_account",
+    )
+    webbrowser.open_new_tab(uri)
+    grant = input("Paste the code you received here, then press Enter")
+    full_user_creds = await aiogoogle.oauth2.build_user_creds(
+        grant=grant, client_creds=CLIENT_CREDS
+    )
+    print(
+        f"full_user_creds: {pprint.pformat(full_user_creds)}"
+    )
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
